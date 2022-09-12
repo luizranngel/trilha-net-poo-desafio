144a17787800109b031baaf28aa9676f6e9bfd33
Luiz Eduardo Abranches <luiz.abranches@catalde.com>
2022-09-12 19:39:14 -0300

    Implementado sistema e abstração de um celular

--- Program.cs
+++ Program.cs
@@ -1,3 +1,15 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+Console.WriteLine("Smartphone Nokia:");
+Smartphone nokia = new Nokia(numero: "123456", modelo: "C3", imei: "11111", memoria: 64);
+nokia.Ligar();
+nokia.InstalarAplicativo("Uber");
+
+Console.WriteLine("\n");
+
+Console.WriteLine("Smartphone Iphone:");
+Smartphone iphone = new Iphone(numero: "1654321", modelo: "14", imei: "2222", memoria: 128);
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("WhatsApp");
+
+Console.ReadKey();
